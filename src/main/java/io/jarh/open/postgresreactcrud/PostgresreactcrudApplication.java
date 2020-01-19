1b4c6577b3a0c21f30031b95f007552904db6ab5
Jarh Lewis <jarhra96@gmail.com>
2020-01-19 17:57:46 -0400

    Initial Commit. Spring Boot Initializer

---
+++ src/main/java/io/jarh/open/postgresreactcrud/PostgresreactcrudApplication.java
@@ -0,0 +1,13 @@
+package io.jarh.open.postgresreactcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresreactcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresreactcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jarh/open/postgresreactcrud/PostgresreactcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jarh.open.postgresreactcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresreactcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
